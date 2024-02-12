f3693096bcc952ececaa97f6ccb6497c70c12823
Andriikryl <andreyk557@gmail.com>
2024-02-12 17:40:17 +0200

    add is object for gui component

--- src/lib/gui.svelte
+++ src/lib/gui.svelte
@@ -1,3 +1,21 @@
 <script lang="ts">
   export let controls;
+  const entries: any = Object.entries($controls);
+  const isNotEmpty = entries.length > 0;
+
+  const is = {
+    number: (value: any) => typeof value === "number",
+    boolean: (value: any) => typeof value === "boolean",
+    text: (value: any) => typeof value === "string" && !value.startsWith("#"),
+    color: (value: any) => typeof value === "string" && value.startsWith("#"),
+    range: (value: any) => typeof value === "object",
+  };
 </script>
+
+{#if isNotEmpty}
+  <div class="gui">
+    {#each entries as [label, value]}
+      <!-- ... -->
+    {/each}
+  </div>
+{/if}
